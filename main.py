ff9b927f11f94a4481215b03786eebcedee37cd8
Rob Pungello <rob@pdfsystems.com>
2025-07-10 14:50:39 -0400

    Add waits

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import argparse
 import os
+import time
 
 import boto3
 from botocore.client import BaseClient
+
 
 def get_alb_client() -> BaseClient:
     return boto3.client(
@@ -11,6 +13,7 @@ def get_alb_client() -> BaseClient:
         aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
         region_name=os.environ.get('AWS_REGION')
     )
+
 
 def get_target_health(client: BaseClient, arn: str, target: str) -> str:
     health = client.describe_target_health(
@@ -29,22 +32,53 @@ def get_target_health(client: BaseClient, arn: str, target: str) -> str:
 
     return 'unknown'
 
+
+def wait_for_target_health(client: BaseClient, arn: str, target: str, health: str, tries: int = 5,
+                           delay: int = 5) -> bool:
+    for i in range(tries):
+        current_health = get_target_health(client, arn, target)
+        if current_health == health:
+            print(f"Target health is now {health}!")
+            return True
+        else:
+            print(f"Current target health is {current_health}, waiting for {health}... (attempt {i + 1}/{tries})")
+        time.sleep(delay)
+
+    return False
+
+
 parser = argparse.ArgumentParser(
     prog='docker run [...]',
     description='A script to interact with AWS ALB and check target health.'
 )
 parser.add_argument('arn', type=str, help='ARN of the target group to check health for')
 parser.add_argument('target', type=str, help='ID of the target instance to check health for')
+parser.add_argument('--wait-for-healthy', action='store_true', help='Wait for status to be healthy before exiting')
+parser.add_argument('--wait-for-unhealthy', action='store_true', help='Wait for status to be unhealthy before exiting')
 args = parser.parse_args()
 
 main_client = get_alb_client()
-main_health = get_target_health(main_client, args.arn, args.target)
-if main_health == 'healthy':
-    print('Target health is healthy!')
-    exit(0)
-elif main_health == 'unhealthy':
-    print('Target health is unhealthy!')
-    exit(1)
+
+if args.wait_for_healthy:
+    if wait_for_target_health(main_client, args.arn, args.target, 'healthy'):
+        print("Success!")
+    else:
+        print("Failed to reach healthy state.")
+        exit(1)
+elif args.wait_for_unhealthy:
+    if wait_for_target_health(main_client, args.arn, args.target, 'unhealthy'):
+        print("Success!")
+    else:
+        print("Failed to reach unhealthy state.")
+        exit(1)
 else:
-    print(f'Target health is unknown ({main_health})!')
-    exit(2)
+    main_health = get_target_health(main_client, args.arn, args.target)
+    if main_health == 'healthy':
+        print('Target health is healthy!')
+        exit(0)
+    elif main_health == 'unhealthy':
+        print('Target health is unhealthy!')
+        exit(1)
+    else:
+        print(f'Target health is unknown ({main_health})!')
+        exit(2)
